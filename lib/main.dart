3dffad20eb45b97c79c81613376c1f15974c0c20
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-09-24 20:09:27 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:nebat/screens/home_screen.dart';
 import 'package:nebat/services/providers.dart';
 import 'package:provider/provider.dart';
+import 'package:location/location.dart';
 
 void main() async {
   // cameras = await availableCameras();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Location location =  Location();
+
+bool _serviceEnabled =false;
+late PermissionStatus _permissionGranted;
+late LocationData _locationData;
+
+
+
+_serviceEnabled = await location.serviceEnabled();
+if (!_serviceEnabled) {
+  _serviceEnabled = await location.requestService();
+  if (!_serviceEnabled) {
+    return;
+  }
+}
+
+final _permissionGranted = await location.hasPermission();
+if (permissionGranted == PermissionStatus.denied) {
+  permissionGranted = await location.requestPermission();
+  if (permissionGranted != PermissionStatus.granted) {
+    return;
+  }
+}
+
+final _locationData = await location.getLocation();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import location
 import modal_progress_hud_nsn
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
 }
