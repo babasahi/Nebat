cb2c9e739952fe9d19921100dcbe28e8e7e2a0aa
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-09-25 08:07:26 +0100

    removed async flag from main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 // ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:nebat/screens/home_screen.dart';
 import 'package:nebat/services/providers.dart';
 import 'package:provider/provider.dart';
 import 'package:location/location.dart';
 
-void main() async {
-  // cameras = await availableCameras();
+void main() {
   runApp(const MyApp());
 }
 
