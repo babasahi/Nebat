73c871645b9c32ad408fa7f5115ae87449723bd1
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-10-04 19:05:00 +0100

    disabled linux and macos support

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -4,45 +4,26 @@ class Plant {
   final String imagePath;
   final double probability;
   final List<String> images;
-  final List<Suggestion> suggestions;
 
   Plant(
       {required this.id,
       required this.plantName,
       required this.imagePath,
       required this.images,
-      required this.probability,
-      required this.suggestions});
+      required this.probability});
 
   Plant.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         plantName = json['plantName'],
         imagePath = json['imagePath'],
         images = json['images'],
-        probability = json['probability'],
-        suggestions = json['suggestions'];
+        probability = json['probability'];
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'plantName': plantName,
         'imagePath': imagePath,
         'images': images,
-        'probability': probability,
-        'suggestions': suggestions
+        'probability': probability
       };
-}
-
-class Suggestion {
-  final String id;
-  final String plantName;
-  Suggestion({required this.id, required this.plantName});
-}
-
-class PlantDetails {
-  final double probability;
-  final bool confirmed;
-  PlantDetails({
-    required this.probability,
-    required this.confirmed,
-  });
 }
